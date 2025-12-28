cd4b8ceb544a6eebd96e03636f80351d0ab24c72
Patel Ved <pved311206@gmail.com>
2025-12-28 09:32:07 +0530

    first-time

---
+++ templates/Front-page/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".auto-type" , {
+    strings : [ "Learn" , "Create" ],
+    typeSpeed : 100,
+    backSpeed : 100,
+    loop : true,
+    showCursor: true,
+    cursorChar: "|"
+})
