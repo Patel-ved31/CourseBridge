d7079f8612f430163894a6dd342716faa67af16c
Patel Ved <pved311206@gmail.com>
2026-02-09 09:12:11 +0530

    Remove deprecated files and code for a cleaner project structure

--- Project/templates/Front-page/script.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".auto-type" , {
-    strings : [ "Learn" , "Create" ],
-    typeSpeed : 100,
-    backSpeed : 100,
-    loop : true,
-    showCursor: true,
-    cursorChar: "|"
-})
